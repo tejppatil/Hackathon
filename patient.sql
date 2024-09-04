485f2292754b796edbf6c869e71de3c16de7e7a4
JAMPANI KOMAL <153545056+JAMPANIKOMAL@users.noreply.github.com>
2024-09-04 22:47:08 +0530

    Add files via upload

---
+++ patient.sql
@@ -0,0 +1,16 @@
+INSERT INTO HOSPITAL
+(DR_NAME,USERNAME,PASS_WORD,PHON_NO_1,PHONE_NO_2,MAIL,ADDRESS,CITIZEN_ID,NMCID,BLOOD_GROUP,UNIQUE_ID,DEGREE,SPECIALIAZATION,HOSPITAL)
+VALUES
+('Dr. Hitalkumar patel',NULL,NULL,'9898989898','8787878787','a1@gmail.com','Rayasan, Landmark: Above Crush Coffee, Gandhinagar',NULL,NULL,NULL,'MBBS, MD','Dermatology , Venereology & Leprosy','The Skin Artistry'),
+('Dr. Heena',NULL,NULL,NULL,NULL,NULL,'Shop Number B 35, Ground Floor, Parmukh Arcade, Reliance Cross Road, Landmark: reliance cross road, Gandhinagar',NULL,NULL,NULL,'BDS','Dental Surgeon,Dentist,Cosmetic/Aesthetic Dentist','Dr. Shukal Dental Clinic'),
+('Dr. Tejasvi Patel',NULL,NULL,NULL,NULL,NULL,'Rayasan, Landmark: Above Crush Coffee, Gandhinagar',NULL,NULL,NULL,'MBBS, MD ','"Dermatology ,Cosmetologist,Dermatosurgeon','The Skin Artistry'),
+('Dr. Diva Sharma',NULL,NULL,NULL,NULL,NULL,'Plot Number.314/317,Shopping Center, Landmark: Above Aya Hotel, Gandhinagar',NULL,NULL,NULL,'MBBS, MS - ENT','ENT/ Otorhinolaryngologist','Radhe Multispeciality Hospital'),
+('Dr. Dipesh Fataniya',NULL,NULL,NULL,NULL,NULL,'Gandhinagar Bypass Road, Sargasan, Landmark: Near N.I.D. & Daiict Campus, Gandhinagar',NULL,NULL,NULL,'MBBS, MD',' Anaesthesiology','Aashka Multispeciality Hospital'),
+('Dr. Jaydeep Kanubhai Ninama',NULL,NULL,NULL,NULL,NULL,'78, Krushna Nagar ,Kankrol Road, Landmark: Near Jain Acharya School, Gandhinagar',NULL,NULL,NULL,'MBBS','General Physician','Shivay Clinic'),
+('Dr. Narendra Parmar',NULL,NULL,NULL,NULL,NULL,'B-208, Vrundavan Trade Center, Gandhinagar, Landmark: Kanishka Speech and Hearing Clinic, Gandhinagar',NULL,NULL,NULL,'MD - Medicine & Therapeutics, MBBS','General Physician','Medicare Hospital'),
+('Dr. Priyanka P Jena',NULL,NULL,NULL,NULL,NULL,'Sector 12, Gandhinagar GMERS Gandhinagar, Gandhinagar',NULL,NULL,NULL,'MBBS','General Physician','GMERS Gandhinagar');
+
+
+
+
+
